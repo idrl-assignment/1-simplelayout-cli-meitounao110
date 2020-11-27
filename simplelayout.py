1174089c4646304708a688507d9e3116e85a1fd4
zyy <431041317@qq.com>
2020-11-27 09:41:49 +0800

    test

--- simplelayout.py
+++ simplelayout.py
@@ -10,24 +10,26 @@ def main():
                         help=' Rectangular component resolution ', type=int)
     parser.add_argument('--unit_n', help=' Numbers of component ', type=int)
     parser.add_argument('--positions',
-                        help=' position serial number of component ', nargs='+', type=int)
+                        help=' position serial number of component ',
+                        nargs='+', type=int)
     parser.add_argument('-o', '--outdir',
-                        help=' directory of output ', default='./example_dir', type=str)
-    parser.add_argument('--file_name', help=' file name of output ', default='example', type=str)
+                        help=' directory of output ',
+                        default='./example_dir', type=str)
+    parser.add_argument('--file_name', help=' file name of output ',
+                        default='example', type=str)
     args = parser.parse_args()
     # 检验是否被整除
-    if args.unit_grid % args.board_grid != 0:
+    if args.board_grid % args.unit_grid != 0:
         return
     uplimit = (args.board_grid / args.unit_grid) ** 2
     # 检验长度是否标准
     if len(args.positions) != args.unit_n:
         return
-    else:
-        for i in range(0, len(args.positions)):
-            if 1 < args.positions[i] or args.positions[i] > uplimit:
-                return
-            else:
-                print(i)
+    for i in range(0, len(args.positions)):
+        if 1 < args.positions[i] or args.positions[i] > uplimit:
+            return
+        else:
+            print(i)
     path = transpath(args.outdir)
     if not os.path.exists(path):
         os.makedirs(path)
@@ -35,7 +37,9 @@ def main():
     full_path = path + args.file_name + '.jpg'
     file = open(full_path, 'w')
     file.write()
-    os.mknod(rf'{args.file_name}.mat')
+    path_full = path + args.file_name + '.mat'
+    os.mknod(rf'{path_full}')
+
 
 def transpath(path):
     path = path.replace('/', 'os.sep')
